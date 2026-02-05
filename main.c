55b9f7edb61a38ffc2c34fe303267f84f23f345f
pifonsec <pm.fonseca16100@gmail.com>
2026-02-05 09:51:34 +0100

    Add files via upload

--- main.c
+++ main.c
@@ -1,26 +1,17 @@
+#include "get_next_line.h"
 #include <fcntl.h>
 #include <stdio.h>
-#include "get_next_line.h"
 
-int main(int argc, char **argv)
+int			main(int ac, char **av)
 {
-    int     fd;
-    char    *line;
+	int		fd;
+	char	*line;
 
-	(void)argc;
-    fd = open(argv[1], O_RDONLY);
-
-    line = get_next_line(fd);
-    printf("1 >>> %s", line);
-    free(line);
-
-    line = get_next_line(fd);
-    printf("2 >>> %s", line);
-    free(line);
-
-    line = get_next_line(fd);
-    printf("3 >>> %s", line);
-    free(line);
-
-    close(fd);
+	line = NULL;
+	fd = open(av[ac-1], O_RDONLY);
+	while (get_next_line(fd, &line))
+	{
+		ft_putendl(line);
+	}
+	close(fd);
 }
