6a4be8f6fd17eee747d75bedd993444901bd7a7e
pifonsec <pm.fonseca16100@gmail.com>
2026-02-05 12:20:10 +0100

    Add files via upload

--- main.c
+++ main.c
@@ -1,17 +1,41 @@
-#include "get_next_line.h"
-#include <fcntl.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <limits.h>
+#include <unistd.h>
+#include "get_next_line.h"
 
-int			main(int ac, char **av)
+int	main(int argc, char **argv)
 {
 	int		fd;
 	char	*line;
+	FILE	*file;
+	char	*test;
+	size_t	len;
 
-	line = NULL;
-	fd = open(av[ac-1], O_RDONLY);
-	while (get_next_line(fd, &line))
+	printf("---------------------------\n");
+	printf("BUFFER_SIZE\t=\t%llu\n", (unsigned long long)BUFFER_SIZE);
+	printf("---------------------------\n");
+
+	if (argc != 2)
 	{
-		ft_putendl(line);
+		printf("Pas de fichier a lire.\n");
+		return (1);
+	}
+
+	/* ===== TEST get_next_line ===== */
+	fd = open(argv[1], O_RDONLY);
+	if (fd == -1)
+	{
+		perror("open");
+		return (1);
+	}
+
+	printf("===== get_next_line =====\n");
+	while ((line = get_next_line(fd)) != NULL)
+	{
+		printf("%s", line);
+		free(line);
 	}
 	close(fd);
 }
